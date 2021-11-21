19212c4e81f314eaa6b2cd33612c33e7230bd9bb
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-21 13:45:08 -0300

    add component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BsCart3 } from "react-icons/Bs";
+import { GiCharacter } from "react-icons/gi";
+
+import { Headers } from "./styles";
+
+// import Image from "next/image";
+// import profilePic from "../../../public/headerLogo.svg";
+import router from "next/dist/client/router";
+
+interface HeaderProps {
+  onOpenModalCart: () => void
+}
+
+const Header = ({ onOpenModalCart }: HeaderProps): JSX.Element => {
+  return (
+    <>
+      <Headers>
+        {/* <Image src={profilePic} alt="logo" width={50} height={50} /> */}
+        <h1 onClick={() => router.push("/")}>
+          Ig<span>.</span>Store
+        </h1>
+
+        <div >
+          <p onClick={() => router.push("/login")}>
+            <GiCharacter /> Conta{" "}
+          </p>
+
+          <button onClick={() => onOpenModalCart()}>
+            <BsCart3 />
+          </button>
+        </div>
+      </Headers>
+    </>
+  );
+};
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+const Headers = styled.header`
+  background-color: #121214;
+  height: 5rem;
+  color: #FFFFFF;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 2rem 10rem;
+
+  border-bottom: 1px solid #3498db;
+
+  h1 {
+    margin-left: 2rem;
+    padding: 0.25rem 0 0.25rem 2rem;
+    border-left: 1px solid #3498db;
+  }
+
+  span {
+    color: #3498db;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p {
+      padding-right: 28px;
+      /* padding-left: 18px; */
+      border-right: 1px solid #3498db;
+    }
+
+    button {
+      svg {
+        margin-left: 28px;
+        color: #3498db;
+        height: 20px;
+        width: 20px;
+      }
+
+    }
+  }
+
+`
+export { Headers }
