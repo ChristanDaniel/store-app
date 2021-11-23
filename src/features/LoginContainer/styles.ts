d4beda87341bd023d78148163d78083360f6a987
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-23 19:42:14 -0300

    css page Login

---
+++ src/features/LoginContainer/styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const LoginMainContainer = styled.main`
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoginInputContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const LoginButtonContent = styled.div`
+  display: flex;
+`
+
+const LoginCartContent = styled.div`
+  display: flex;
+`
+
+
+export { LoginMainContainer, LoginButtonContent, LoginInputContent, LoginCartContent };
