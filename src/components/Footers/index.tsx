86f3a26ba2144fd61ae220a75e655e17b7e0b278
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-25 17:00:39 -0300

    Component Footers criado

---
+++ src/components/Footers/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, ContainerRedeSociais, ContentRedeSociais } from "./styles";
+
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export function Footers(): JSX.Element {
+  return (
+    <>
+      <Footer>
+        
+      </Footer>
+    </>
+  );
+}
