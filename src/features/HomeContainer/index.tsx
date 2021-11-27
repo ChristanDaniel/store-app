005f8059e560c48673c8f04984578600a20e0c7b
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-27 13:46:01 -0300

    recebendo dados da API

--- src/features/HomeContainer/index.tsx
+++ src/features/HomeContainer/index.tsx
@@ -1,20 +1,48 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useContext, useEffect, useState } from "react";
+import axios from 'axios';
+import { useRouter } from 'next/dist/client/router';
+import React, { useContext, useEffect, useState } from 'react'
+import { Footers } from '../../components/Footers';
 
-// import { MainContainer, Banner, MainContent, MainContentDiv } from "./styles";
+import { MainContainer, Banner, MainContent, MainContentDiv } from "./styles";
 
+interface ProductProps {
+  category?: string;
+  description?: string;
+  id: number;
+  image: string;
+  price: number;
+  rating?: [
+    {
+      rate: string;
+      count: string;
+    }
+  ];
+  title: string;
+  count: number
+}
 
 const HomeContainer = (): JSX.Element => {
-  const [teste, setTeste] = useState("");
+  const [teste, setTeste] = useState('')
+  const [products, setProducs] = useState<ProductProps[]>([]);
 
-  useEffect(() => {}, []);
+  const router = useRouter()
+
+  const getAllProducts = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    console.log(response)
+    setProducs(response.data);
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+  useEffect(() => {}, [])
 
   return (
     <>
-      <main>
-        <h1>Hello world</h1>
-      </main>
+     
     </>
-  );
-};
-export default HomeContainer;
+  )
+}
+export default HomeContainer
