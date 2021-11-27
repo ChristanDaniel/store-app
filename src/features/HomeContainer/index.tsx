97d767ecd120a652a6a6b5b2a2d8159659a0b3fa
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-27 13:46:20 -0300

    mostrando em tela dados da api

--- src/features/HomeContainer/index.tsx
+++ src/features/HomeContainer/index.tsx
@@ -41,7 +41,26 @@ const HomeContainer = (): JSX.Element => {
 
   return (
     <>
-     
+      <MainContainer>
+        <Banner src="https://img.terabyteshop.com.br/banner/1200.jpg" alt="teste" />
+        <MainContent>
+          {products.map((product, index) => {
+            return (
+              <MainContentDiv key={product.id + index}>
+                <img src={`${product.image}`} alt="Mouse" />
+
+                {/* <button onClick={() => handleAddCart(product)}>Adicionar no Carrinho</button> */}
+                <h3>{product.title}</h3>
+                <span>${product.price}</span>
+                <p>{product.price}</p>
+                <p>ou até 6x de R$ 50,00</p>
+                <button onClick={() => router.push(`/Product/${product.id}`)}>Comprar Agora</button>
+              </MainContentDiv>
+            )
+          })}
+        </MainContent>
+        <Footers />
+      </MainContainer>
     </>
   )
 }
