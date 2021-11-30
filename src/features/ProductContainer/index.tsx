6383ff591854cfd135db631c3b21a94ee07e1a71
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-29 21:28:17 -0300

    req api product

--- src/features/ProductContainer/index.tsx
+++ src/features/ProductContainer/index.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/dist/client/router";
 
 import { MainProductContainer } from "./styles";
-import { ProductContainerContext } from "../ProductContainerContext";
+// import { ProductContainerContext } from "../ProductContainerContext";
 import axios from "axios";
 
 interface ProductProps {
@@ -23,11 +23,34 @@ interface ProductProps {
 }
 
 const HomeProductContainer = (): JSX.Element => {
+  // const { products } = useContext(ProductContainerContext);
 
+  const [Product, setProduct] = useState<ProductProps>();
+  const router = useRouter();
+
+  const getProduct = async () => {
+    if (!router.query.id) {
+      router.push('/')
+    }
+
+    const response = await axios.get(
+      `https://fakestoreapi.com/products/${router.query.id}`
+    );
+    setProduct(response.data);
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
   return (
     <>
       <MainProductContainer>
-
+      <div>
+          <div>
+            <img src={`${Product?.image}`} alt={`${Product?.description}`} />
+            <h1>{Product?.title}</h1>
+          </div>
+        </div>
       </MainProductContainer>
     </>
   );
