85dd8402d19286f7c757f5f515c63226c7ade077
ChristianDaniel <christian_daniel04@hotmail.com>
2021-11-30 22:23:26 -0300

    create CartReducer

---
+++ src/features/CartReducer.ts
@@ -0,0 +1,41 @@
+export type Reducer<State, Action> = (state: ProductState[], action: ProductAction) => State
+
+export const initialState: ProductState = {
+  id: 0,
+  title: '',
+  image: '',
+  price: 0,
+  count: 0
+}
+
+export interface ProductState {
+  id: number
+  title: string
+  image: string
+  price: number
+  count: number
+}
+
+export type ProductAction = {
+  type: 'init_stored'
+  id: number
+  title: string
+  image: string
+  price: number
+  count: number
+}
+
+export const AppReducer = (state: ProductState[], action: ProductAction) => {
+  switch (action.type) {
+    case 'init_stored': {
+      return {
+        id: action.id,
+        title: action.title,
+        image: action.image,
+        price: action.price
+      }
+    }
+    default:
+      return state
+  }
+}
