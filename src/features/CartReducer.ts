1a82bfa5088fee9f557694a6b92f122718ef0274
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-01 17:30:21 -0300

    melhorando CartReducer

--- src/features/CartReducer.ts
+++ src/features/CartReducer.ts
@@ -1,41 +1,66 @@
-export type Reducer<State, Action> = (state: ProductState[], action: ProductAction) => State
+export type Reducer<State, Action> = (
+  state: ProductState[],
+  action: ProductAction
+) => State;
 
 export const initialState: ProductState = {
   id: 0,
-  title: '',
-  image: '',
+  title: "",
+  image: "",
   price: 0,
-  count: 0
-}
+  count: 0,
+};
 
 export interface ProductState {
-  id: number
-  title: string
-  image: string
-  price: number
-  count: number
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  count: number;
 }
 
-export type ProductAction = {
-  type: 'init_stored'
-  id: number
-  title: string
-  image: string
-  price: number
-  count: number
-}
+export type ProductAction =
+  | {
+      type: "init_stored";
+      id: number;
+      title: string;
+      image: string;
+      price: number;
+      count: number;
+    }
+  | {
+      type: "add_product";
+      id: number;
+      title: string;
+      image: string;
+      price: number;
+      count: number;
+    };
 
 export const AppReducer = (state: ProductState[], action: ProductAction) => {
   switch (action.type) {
-    case 'init_stored': {
+    case "init_stored": {
       return {
         id: action.id,
         title: action.title,
         image: action.image,
-        price: action.price
-      }
+        price: action.price,
+      };
     }
+    case "add_product": {
+      return [
+        ...state,
+        {
+          id: action.id,
+          title: action.title,
+          image: action.image,
+          price: action.price,
+          count: action.count,
+        },
+      ];
+    }
+
     default:
-      return state
+      return state;
   }
-}
+};
