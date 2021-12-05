e8812b539c2ec91064d68fa1478a3858e82cb3ad
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-05 14:30:07 -0300

    create css compoent OpenModalCart

---
+++ src/components/OpenModalCart/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const ModalContainer = styled.div`
+  display: flex;
+`
+
+const HeadModalContent = styled.div``
+
+const BodyModalContent = styled.div``
+
+const AmountProductContent = styled.div``
+
+export { ModalContainer, HeadModalContent, BodyModalContent, AmountProductContent }
