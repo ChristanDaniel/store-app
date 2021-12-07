21b8b100014bb5be5fcfd3a93e9ef45f881e6edb
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-07 18:49:29 -0300

    add css Footer

--- src/components/Footers/styles.ts
+++ src/components/Footers/styles.ts
@@ -4,43 +4,59 @@ const Footer = styled.footer`
   width: 100%;
   background: #000;
   display: flex;
-  align-items: center;
-  justify-content: space-around;
-  h3 {
-    color: #fff
-  }
-  div {
-    /* margin: 0 0 8px 80px; */
-  }
-
-  p {
-    color: #fff;
-    /* margin-bottom: 12px; */
-    font-size: smaller;
-  }
-  a {
-    display: flex;
-    background: #fff;
-    color: inherit;
-    text-decoration: underline;
-  }
-
+  color: white;
+  flex-direction: column;
+  align-items:center;
+  padding-top: 18px;
+  -webkit-box-shadow: -1px -3px 11px 2px rgba(0,0,0,0.24);
+  box-shadow: -1px -3px 11px 2px rgba(0,0,0,0.24);
+  border-top: 2px solid #3498db;
 `;
 
-const ContainerRedeSociais = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const ContentRedeSociais = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 15px;
-
-  div {
-    padding: 8px;
-    display: flex;
-    flex-direction: row;
+const SecurityAndPayment= styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 1100px;
+  h1 {
+    margin-left: 8px;
   }
 `
 
-export { Footer, ContainerRedeSociais, ContentRedeSociais };
+const PaymentMethod = styled.div`
+  display:flex;
+  flex-direction: row;
+  gap: 8px
+
+
+  h1 {
+    margin-left: 8px;
+  }
+`
+
+const Security = styled.div`
+  display:flex;
+  flex-direction: row;
+
+  gap: 20px
+`
+
+const SocialsRede = styled.div`
+  margin-left: 30px;
+
+  svg {
+    height: 48px;
+    width: 48px;
+  }
+
+ #social-icon{
+   margin: 10px;
+   cursor: pointer;
+
+   :hover {
+     opacity: 0.8;
+   }
+ }
+`
+
+export { Footer, PaymentMethod, SecurityAndPayment, Security, SocialsRede};
