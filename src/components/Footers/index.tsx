3413565cbb7a0a80085151d8260f0d49220af845
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-07 18:48:37 -0300

    add image footer

--- src/components/Footers/index.tsx
+++ src/components/Footers/index.tsx
@@ -1,63 +1,62 @@
 import React from "react";
-import { Footer, ContainerRedeSociais, ContentRedeSociais } from "./styles";
+import {
+  Footer,
+  PaymentMethod,
+  Security,
+  SecurityAndPayment,
+  SocialsRede,
+} from "./styles";
+import { FaLinkedin, FaGithubSquare, FaFacebookSquare } from "react-icons/fa";
+import Link from "next/link";
 
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Image from "next/image";
 
 export function Footers(): JSX.Element {
   return (
-    <>
-      <Footer>
-        <div>
-          <h3>Páginas</h3>
-          <div>
-            <a>Home</a>
-            <a>Cart</a>
-            <a>Login</a>
-          </div>
-        </div>
-        <ContainerRedeSociais>
-          <h3>Redes Sociais</h3>
-          <ContentRedeSociais>
-            <div>
-              <a
-                href="https://github.com/ChristanDaniel"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <FaGithub /> Github
-              </a>
-              <p> | </p>
-              <a
-                href="https://www.linkedin.com/in/christian-daniel-841921210/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                <FaLinkedin /> Linkedin
-              </a>
-            </div>
-            <div>
-              <p>
-                <span>© {new Date().getFullYear()} Ig-Store</span>
-                {" · "}
-                <span>Construido por Christian Daniel .</span>
-              </p>
-            </div>
-            <div>
-              <h3>Pagamentos</h3>
-              <p>VISA</p>
-            </div>
-          </ContentRedeSociais>
-        </ContainerRedeSociais>
-        <div>
-          <h3>Entre em contato</h3>
-          <p>Nome: Ig.Store</p>
-          <p>CNPJ: 12.456.789/0001-02</p>
-          <p>E-mail: contato@igStore.com</p>
-          <p>Telefone: (34) 3812380</p>
-          <p>Rua: Bela Vista, 123 - Centro</p>
-          <p>Uberlândia / São Paulo - 38740-082</p>
-        </div>
-      </Footer>
-    </>
+    <Footer>
+      <SecurityAndPayment>
+        <PaymentMethod>
+          <h1>
+            <img
+              src="https://img.terabyteshop.com.br/cartoes/fpr_mercadopago.png"
+              alt="foto"
+            />
+          </h1>
+          <h1>
+            <img
+              src="https://img.terabyteshop.com.br/cartoes/fpr_pixMP.png"
+              alt="foto"
+            />
+          </h1>
+          <h1>
+            <img
+              src="https://img.terabyteshop.com.br/cartoes/fpr_boleto.png"
+              alt="foto"
+            />
+          </h1>
+        </PaymentMethod>
+        <Security>
+          <h1>
+            <img
+              src="https://img.terabyteshop.com.br/selos/ra-1000.png"
+              alt="foto"
+            />
+          </h1>
+          <h1>
+            <img
+              src="https://img.terabyteshop.com.br/selos/compre-confie.png"
+              alt="foto"
+            />
+          </h1>
+          <h1>
+            <img
+              src="https://img.terabyteshop.com.br/selos/google-site-seguro.png"
+              alt="foto"
+            />
+          </h1>
+        </Security>
+      </SecurityAndPayment>
+
+    </Footer>
   );
 }
