405587bfed13fa36e806e1adf8a9624b5b0bfaad
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-06 22:34:22 -0300

    css component OpenModalCart

--- src/components/OpenModalCart/styles.ts
+++ src/components/OpenModalCart/styles.ts
@@ -1,13 +1,65 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const ModalContainer = styled.div`
   display: flex;
-`
+  flex-direction: column;
+  height: 450px;
+  /* margin: 20px; */
 
-const HeadModalContent = styled.div``
 
-const BodyModalContent = styled.div``
+`;
 
-const AmountProductContent = styled.div``
+const HeadModalContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* margin: 20px; */
 
-export { ModalContainer, HeadModalContent, BodyModalContent, AmountProductContent }
+  button {
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    height: 18px;
+    width: 18px;
+  }
+`;
+
+const BodyModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+  margin-top: 15px;
+  background: #DCDCDC;
+
+  h4 {
+    font-size: 13px;
+  }
+
+  div {
+    width: 530px;
+    /* margin: 10px; */
+    display: flex;
+    justify-content: space-between;
+
+    span {
+      color: #737380;
+      text-decoration: line-through;
+    }
+  }
+`;
+
+const ProductDivContent = styled.div``;
+
+const AmountProductContent = styled.div``;
+
+const FooterModalContent = styled.div``;
+
+export {
+  ModalContainer,
+  HeadModalContent,
+  BodyModalContent,
+  ProductDivContent,
+  AmountProductContent,
+  FooterModalContent,
+};
