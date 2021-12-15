43c88d8f8e800b32c385f75585c12820b1692c5f
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-14 22:47:59 -0300

    html productContainer

--- src/features/ProductContainer/index.tsx
+++ src/features/ProductContainer/index.tsx
@@ -45,12 +45,38 @@ const HomeProductContainer = (): JSX.Element => {
   return (
     <>
       <MainProductContainer>
-      <div>
-          <div>
-            <img src={`${Product?.image}`} alt={`${Product?.description}`} />
-            <h1>{Product?.title}</h1>
-          </div>
-        </div>
+        <GeneralInfo>
+          <hr />
+          <h1> Características Gerais </h1>
+          <hr />
+        </GeneralInfo>
+
+          <ProductInfo>
+            <ProductDescription>
+              <h3>{Product?.title}</h3>
+              <ProdCategory>Categoria: {Product?.category}</ProdCategory>
+              <ProdDelivery>
+                <BsFillLightningChargeFill /> Pronta Entrega
+              </ProdDelivery>
+              <p>{Product?.description}</p>
+              <AllRate>
+                ★★★★★ <span>{Product?.rating.count} avaliações</span>
+              </AllRate>
+              <ProductPriceValue>
+                R$ {Product?.price.toFixed(2)}
+              </ProductPriceValue>
+              <ProductInstallments>
+                <BsFillCreditCardFill /> 3x de{" "}
+                {FormatedValues("dividido", Product?.price)} sem juros no
+                cartão.
+              </ProductInstallments>
+
+            </ProductDescription>
+            <ProductImage>
+              <img src={`${Product?.image}`} alt={`${Product?.description}`} />
+            </ProductImage>
+          </ProductInfo>
+
       </MainProductContainer>
     </>
   );
