b8bc3643776d9573f77fff9660dd67f9b808fef2
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-16 15:59:19 -0300

    add css Login

--- src/features/LoginContainer/styles.ts
+++ src/features/LoginContainer/styles.ts
@@ -5,7 +5,51 @@ const LoginMainContainer = styled.main`
   height: calc(100vh - 80px);
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
+
+const LoginMessageContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #eee;
+  padding: 25px;
+  z-index: 1;
+  border-radius: 8px;
+
+
+  span {
+    color: #303030;
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: 1px;
+    margin-top: -15px;
+  }
+
+  div {
+    margin-top: 50px
+  }
+`;
+
+
+const LoginMessage = styled.p`
+  font-size: 60px;
+  margin-right: 60px;
+  font-weight: 900;
+  color: #3498db;
+`;
+
+const LoginMessageInformation = styled.div`
+  span {
+    color: #3498db;
+  }
+
+  p {
+    color: #303030;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: normal;
+  }
+`
 
 const LoginInputContent = styled.div`
   display: flex;
@@ -25,4 +69,4 @@ const LoginCartContent = styled.div`
 `
 
 
-export { LoginMainContainer, LoginButtonContent, LoginInputContent, LoginCartContent };
+export { LoginMainContainer, LoginButtonContent, LoginMessageInformation, LoginMessage, LoginInputContent, LoginCartContent, LoginMessageContent };
