d681e280bf07dbf571adc11ec95b0fbe24630fd9
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-17 01:48:59 -0300

    create feature SucessContainer

---
+++ src/features/SucessContainer/index.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+
+
+const SucessContainer = (): JSX.Element => {
+
+  useEffect(() => {}, []);
+
+  return (
+    <>
+       <div>Hello World</div>
+    </>
+  );
+};
+
+export default SucessContainer;
