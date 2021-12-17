f72a71432befb7ac5d17bdc6bb433bf0b837b435
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-17 01:53:10 -0300

    add sucessImage

--- src/features/SucessContainer/index.tsx
+++ src/features/SucessContainer/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 
+import Image from "next/image";
+import profilePic from "../../../public/PedidoConcluído.svg";
 
 
 const SucessContainer = (): JSX.Element => {
@@ -8,7 +10,10 @@ const SucessContainer = (): JSX.Element => {
 
   return (
     <>
-       <div>Hello World</div>
+      <SucessMainContainer>
+        <Image src={profilePic} alt='teste' height={360} width={360}/>
+        <h1>Pedido realizado com sucesso!</h1>
+      </SucessMainContainer>
     </>
   );
 };
