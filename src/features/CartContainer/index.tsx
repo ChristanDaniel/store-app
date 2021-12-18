1f6fd9673b97a9213a10f38710de6bc85bdc55d9
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-18 13:28:40 -0300

    create ProductCartContainer

--- src/features/CartContainer/index.tsx
+++ src/features/CartContainer/index.tsx
@@ -34,14 +34,52 @@ interface ProductProps {
 
 const HomeProductContainer = (): JSX.Element => {
 
+  const { contextValue, loginAuthentication, teste, setTeste } = useContext(
+    ProductContainerContext
+  );
+  const { state } = contextValue;
+
 
   // useEffect(() => {
   // }, []);
 
   return (
     <>
+    <MainCartProducts>
+        <h1>
+        01036-970
+          CARRINHO <BsCart3 />
+        </h1>
+        <PaymenteContainer>
+          <SectionPaymentCartContainer>
+            <ProductCartContainer>
+              {state.map((prod, index) => {
+                return (
+                  <>
+                    <ProductContentCart key={prod.id + index}>
+                      <img src={prod.image} alt={prod.title} />
+                      <h4>{prod.title}</h4>
+                      <div>
+                        <p>Quantidade</p>
+                        <div>
+                          <ButtonReduce onClick={() => decrementar(prod.count, prod.id, prod.price)}>-</ButtonReduce>
+                          <p>{prod.count}</p>
+                          <ButtonAdd onClick={() => incrementar(prod.count, prod.id, prod.price)}>+</ButtonAdd>
+                        </div>
+                      </div>
+                      <ProductPrice>R${prod.price.toFixed(2)}</ProductPrice>
+                      <button onClick={() => handleDeleteProductFavoriteCart(index)}><CgTrashEmpty /></button>
+                    </ProductContentCart>
+                  </>
+                );
+              })}
+            </ProductCartContainer>
+          </SectionPaymentCartContainer>
 
+        </PaymenteContainer>
+      </MainCartProducts>
     </>
   );
 };
+
 export default HomeProductContainer;
