45ed9a447957f822a261928fdfb97aa61dd03c51
ChristianDaniel <christian_daniel04@hotmail.com>
2021-12-31 03:34:32 -0300

    pequenas alterações

--- src/features/ProductContainer/index.tsx
+++ src/features/ProductContainer/index.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
-import { useRouter } from "next/dist/client/router";
+import React, { useContext, useEffect, useState, useCallback } from 'react'
+import { useRouter } from 'next/dist/client/router'
 import {
   AllRate,
   GeneralInfo,
@@ -13,93 +13,86 @@ import {
   ProductInfo,
   ProductInstallments,
   ProductPriceValue,
-  ProgressContainer,
-} from "./styles";
-import { ProductContainerContext } from "../ProductContainerContext";
-import axios from "axios";
-import { Footers } from "../../components/Footers";
+  ProgressContainer
+} from './styles'
+import { ProductContainerContext } from '../ProductContainerContext'
+import axios from 'axios'
+import { Footers } from '../../components/Footers'
 // import CircularProgress from "@mui/material/CircularProgress";
 // import Box from "@mui/material/Box";
-import {
-  BsFillLightningChargeFill,
-  BsFillCreditCardFill,
-} from "react-icons/bs";
-import { FaBarcode } from "react-icons/fa";
-import Button from "../../components/Button";
+import { BsFillLightningChargeFill, BsFillCreditCardFill } from 'react-icons/bs'
+import { FaBarcode } from 'react-icons/fa'
+import Button from '../../components/Button'
 
 interface ProductProps {
-  category: string;
-  description: string;
-  id: string;
-  image: string;
-  price: number;
+  category: string
+  description: string
+  id: string
+  image: string
+  price: number
   rating: [
     {
-      rate: string;
-      count: string;
+      rate: string
+      count: string
     }
-  ];
-  title: string;
+  ]
+  title: string
 }
 
 const HomeProductContainer = (): JSX.Element => {
-  // const { products } = useContext(ProductContainerContext);
+  const [Product, setProduct] = useState<ProductProps>()
+  const router = useRouter()
 
-  const [Product, setProduct] = useState<ProductProps>();
-  const router = useRouter();
+  const { contextValue } = useContext(ProductContainerContext)
 
   const getProduct = async () => {
     if (!router.query.id) {
       router.push('/')
     }
 
-    const response = await axios.get(
-      `https://fakestoreapi.com/products/${router.query.id}`
-    );
-    setProduct(response.data);
-  };
+    const response = await axios.get(`https://fakestoreapi.com/products/${router.query.id}`)
+    setProduct(response.data)
+  }
 
   const handleCheckIfHasOnCart = (productId: number) => {
-    const hasProduct = Boolean(
-      contextValue.state.find((prod) => prod.id === productId)
-    );
+    const hasProduct = Boolean(contextValue.state.find((prod) => prod.id === productId))
 
-    console.log("hasProduct", hasProduct);
-    return hasProduct;
-  };
+    console.log('hasProduct', hasProduct)
+    return hasProduct
+  }
 
   const handleAddCart = (product: ProductProps) => {
-    const hasProduct = handleCheckIfHasOnCart(product.id);
+    const hasProduct = handleCheckIfHasOnCart(product.id)
 
     !hasProduct &&
       contextValue.dispatch({
-        type: "add_product",
+        type: 'add_product',
 
         id: product.id,
         title: product.title,
         image: product.image,
         price: product.price,
-        count: 1,
-      });
+        count: 1
+      })
 
-    router.push("/Cart");
-  };
+    router.push('/Cart')
+  }
 
   const FormatedValues = (ind: string, value: number) => {
-    if (ind === "desconto") {
-      const num = value * 0.9;
-      return num.toFixed(2);
+    if (ind === 'desconto') {
+      const num = value * 0.9
+      return num.toFixed(2)
     }
 
-    if (ind === "dividido") {
-      const num = value / 3;
-      return num.toFixed(2);
+    if (ind === 'dividido') {
+      const num = value / 3
+      return num.toFixed(2)
     }
-  };
+  }
 
   useEffect(() => {
-    getProduct();
-  }, []);
+    getProduct()
+  }, [])
 
   return (
     <>
@@ -122,27 +115,16 @@ const HomeProductContainer = (): JSX.Element => {
               <AllRate>
                 ★★★★★ <span>{Product?.rating.count} avaliações</span>
               </AllRate>
-              <ProductPriceValue>
-                R$ {Product?.price.toFixed(2)}
-              </ProductPriceValue>
+              <ProductPriceValue>R$ {Product?.price.toFixed(2)}</ProductPriceValue>
               <ProductInstallments>
-                <BsFillCreditCardFill /> 3x de{" "}
-                {FormatedValues("dividido", Product?.price)} sem juros no
-                cartão.
+                <BsFillCreditCardFill /> 3x de {FormatedValues('dividido', Product?.price)} sem juros no cartão.
               </ProductInstallments>
-              <ProductCodeValue>
-                R$ {FormatedValues("desconto", Product?.price)}
-              </ProductCodeValue>
+              <ProductCodeValue>R$ {FormatedValues('desconto', Product?.price)}</ProductCodeValue>
               <ProductCode>
                 <FaBarcode /> à vista com 10% de desconto no boleto ou pix
               </ProductCode>
-              <Button
-                $disabled={handleCheckIfHasOnCart(Product.id)}
-                onClick={() => handleAddCart(Product)}
-              >
-                {handleCheckIfHasOnCart(Product.id)
-                  ? "Produto adicionado ao carrinho"
-                  : "Comprar Agora"}
+              <Button $disabled={handleCheckIfHasOnCart(Product.id)} onClick={() => handleAddCart(Product)}>
+                {handleCheckIfHasOnCart(Product.id) ? 'Produto adicionado ao carrinho' : 'Comprar Agora'}
               </Button>
             </ProductDescription>
             <ProductImage>
@@ -162,6 +144,6 @@ const HomeProductContainer = (): JSX.Element => {
         <Footers />
       </MainProductContainer>
     </>
-  );
-};
-export default HomeProductContainer;
+  )
+}
+export default HomeProductContainer
