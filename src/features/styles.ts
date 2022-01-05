fd8537142cad37af57b9a1b780096acd47004736
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-05 20:47:05 -0300

    add styles de ProductContainer

---
+++ src/features/styles.ts
@@ -0,0 +1,11 @@
+import styled, { css } from "styled-components";
+
+const pageContainer = css`
+  padding-top: 80px;
+  height: calc(100vh - 80px);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export { pageContainer }
