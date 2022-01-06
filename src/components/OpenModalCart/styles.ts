1363da0e950d0f57bba1a410934bf8ac1d01de3d
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-06 17:34:02 -0300

    melhorando css Modal

--- src/components/OpenModalCart/styles.ts
+++ src/components/OpenModalCart/styles.ts
@@ -4,19 +4,48 @@ const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 450px;
-  /* margin: 20px; */
-
-
 `;
 
 const HeadModalContent = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   /* margin: 20px; */
+
+  div{
+    display: flex;
+    align-items: center;
+  }
+
+  span {
+    margin-left: 20px;
+    color: #fff;
+    background: #64a5f0;
+    padding: 8px;
+    border-radius: 6px;
+  }
 
   button {
     display: flex;
     align-items: center;
+
+    border-radius: 8px;
+    background: #64a5f0;
+    color: #fff;
+    padding: 10px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    cursor: pointer;
+    border: 0;
+
+    transition: 0.2s;
+
+    :hover {
+      filter: brightness(0.9);
+    }
   }
 
   svg {
@@ -27,37 +56,19 @@ const HeadModalContent = styled.div`
 
 const BodyModalContent = styled.div`
   display: flex;
+  height: 300px;
   flex-direction: column;
   overflow: auto;
-  margin-top: 15px;
-  background: #DCDCDC;
+  margin-top: 8px;
+  background: #dcdcdc;
 
   h4 {
     font-size: 13px;
   }
-
-  div {
-    width: 530px;
-    /* margin: 10px; */
-    display: flex;
-    justify-content: space-between;
-
-    span {
-      color: #737380;
-      text-decoration: line-through;
-    }
-  }
 `;
 
-const ProductDivContent = styled.div`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid #3498db;
-  margin-top: 20px;
-`;
 
-const AmountProductContent = styled.div`
-  display: flex;
+
 
   button {
     display: flex;
@@ -70,6 +81,20 @@ const AmountProductContent = styled.div`
   }
 `;
 
+const NoValueCart = styled.div`
+  display: flex;
+  flex: 3;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+
+  svg {
+    margin: 10px;
+    height: 100px;
+    width: 100px;
+  }
+`;
+
 const FooterModalContent = styled.div`
   display: flex;
   flex: 1;
@@ -78,9 +103,12 @@ const FooterModalContent = styled.div`
   flex-direction: column;
 
   div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     margin: 10px;
 
-    p {
+    h2 {
       color: #006400;
     }
   }
@@ -90,6 +118,7 @@ export {
   ModalContainer,
   HeadModalContent,
   BodyModalContent,
+  NoValueCart,
   ProductDivContent,
   AmountProductContent,
   FooterModalContent,
