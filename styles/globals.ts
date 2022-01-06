6545697d0ba55909b5789237febc1d403fbe4f51
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-06 16:03:35 -0300

    css global Modal

--- styles/globals.ts
+++ styles/globals.ts
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
-import { pageContainer } from "../src/features/styles";
+import { pageContainer } from '../src/features/styles'
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -44,4 +44,44 @@ export const GlobalStyle = createGlobalStyle`
   h1, h2, h3, h4, h5, h6, strong {
     font-weight: 600;
   }
+  .react-modal-overlay {
+    background-color: rgba(0, 0, 0, 0.8);
+    z-index: 2;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-close {
+    padding-left: 450px;
+    position: absolute;
+    left: 134px;
+    top: 16px;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 650px;
+    background: #eee;
+
+    padding: 50px 50px 20px 50px;
+    position: relative;
+
+    border-radius: 0.24rem;
+  }
+
 `
