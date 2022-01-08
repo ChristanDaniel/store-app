781defcbf4d09744e5306ced5c4b2b5c185fb31c
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-08 14:47:31 -0300

    add 3 banners

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -10,8 +10,32 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
+import Image from "next/image";
+import BannerOne from "../../../public/bannerOne.jpg";
+import BannerTwo from "../../../public/bannerTwo.jpg";
+import BannerThree from "../../../public/bannerThree.jpg";
+
 export default function Slider() {
   return (
+    <Swiper
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{
+        delay: 4000,
+      }}
+    >
+      <SwiperSlide>
+        <Image src={BannerOne} alt="BannerOne" height={410} />
+      </SwiperSlide>
 
+      <SwiperSlide>
+        <Image src={BannerTwo} alt="BannerTwo" height={410} />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Image src={BannerThree} alt="BannerThree" height={410} />
+      </SwiperSlide>
+    </Swiper>
   );
 }
