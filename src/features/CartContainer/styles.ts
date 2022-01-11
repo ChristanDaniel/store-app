0a36f7e080348d29fc32160b4c0c8beae60b778d
ChristianDaniel <christian_daniel04@hotmail.com>
2022-01-11 19:01:43 -0300

    css Cart Container

--- src/features/CartContainer/styles.ts
+++ src/features/CartContainer/styles.ts
@@ -12,12 +12,16 @@ const MainCartProducts = styled.main`
   background: #eee;
 `;
 
+const MainContentCartProducts = styled.div``;
+
 const PaymenteContainer = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-const SectionPaymentCartContainer = styled.section``;
+const SectionPaymentCartContainer = styled.section`
+  position: relative;
+`;
 
 const ProductCartContainer = styled.div`
   width: 800px;
@@ -26,6 +30,31 @@ const ProductCartContainer = styled.div`
   padding: 0 12px 0 0;
   position: relative;
   top: 20px;
+
+  #clear-cart-button {
+    background-color: transparent;
+    color: #778899;
+    font-size: 11px;
+    opacity: 0.9;
+    margin-left: auto;
+    margin-top: -15px;
+    margin-right: -12px
+  }
+
+  ::-webkit-scrollbar {
+    width: 7px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #fff;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: grey;
+    border-radius: 10px;
+  }
+
 `;
 
 const ProductContentCart = styled.div`
@@ -49,27 +78,42 @@ const ProductContentCart = styled.div`
   }
 
   p {
-    font-size: 12px;
-    padding: 5px;
+    font-size: 14px;
   }
 
   img {
     height: 70px;
     width: 80px;
   }
-
+  /*
   h4 {
-    max-width: 400px;
+    min-width: 400px;
+  } */
+
+  #delete-product {
+    padding: 0;
+    margin: 0;
+    height: 0;
+    background: transparent;
+
+    svg {
+      color: #000;
+    }
   }
 `;
 
+const ProductTitle = styled.p`
+  min-width: 323px;
+`;
+
 const ButtonReduce = styled.button`
-  font-size: 20px;
+  font-size: 24px;
   padding: 2px;
   margin: 4px;
   align-items: center;
   border-radius: 4px;
-  color: #ff000090;
+  color: #00070;
+  background: transparent;
 
   cursor: pointer;
   border: 0;
@@ -77,19 +121,19 @@ const ButtonReduce = styled.button`
   transition: 0.3s;
 
   :hover {
-    filter: brightness(0.9);
-    background: #ff000040;
+    filter: brightness(0.7);
+    /* background: #ff000040; */
   }
 `;
 
-
 const ButtonAdd = styled.button`
-  font-size: 20px;
+  font-size: 19px;
   padding: 2px;
   margin: 4px;
   align-items: center;
   border-radius: 4px;
   color: #00640090;
+  background: transparent;
 
   cursor: pointer;
   border: 0;
@@ -98,7 +142,7 @@ const ButtonAdd = styled.button`
 
   :hover {
     filter: brightness(0.9);
-    background: #00640040;
+    /* background: #00640040; */
   }
 `;
 
@@ -107,6 +151,92 @@ const ProductPrice = styled.span`
   color: #006400;
   min-width: 92px;
 `;
+
+const OrderSummaryContent = styled.div`
+  background-color: white;
+  border-radius: 8px;
+  margin-bottom: 2px;
+  box-shadow: -1px 1px 15px 2px rgba(0, 0, 0, 0.24);
+  position: relative;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    padding: 6px;
+  }
+  p {
+    font-size: 13px;
+  }
+  span {
+    font-size: 13px;
+    /* margin: 0px 0px 0px 0px; */
+    min-width: 100px;
+  }
+`;
+
+const TotalOrderSummary = styled.div`
+  align-items: center;
+  background: #00640020;
+  border-radius: 0px 0px 10px 10px;
+
+  h5 {
+    padding: 8px;
+  }
+`;
+
+const FormOfPaymentContent = styled.div`
+  background-color: white;
+  padding: 12px;
+  /* border-radius: 10px 10px 0px 0px; */
+
+  svg {
+    height: 34px;
+    width: 37px;
+    margin-right: 10px;
+    color: #000;
+  }
+
+  span {
+    font-size: 15px;
+    color: rgba(0,160,0,.1);
+  }
+
+  div {
+    margin: 4px 0px 14px 10px;
+    display: flex;
+  }
+`;
+
+const PaymentCartContent = styled.p`
+  display: flex;
+  align-items: center;
+  color: #808080;
+  font-size: 13px;
+`;
+
+const PaymentTicketContainer = styled.div`
+  svg {
+    margin-top: 8px;
+  }
+`
+
+const PaymentTicketContent = styled.p`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  color: #808080;
+  font-size: 13px;
+
+  svg {
+    margin-top: 14px;
+  }
+`;
+
+const PaymenteTicketTotal = styled.p`
+  font-size: 16px;
+  font-weight: 550;
+  color: #00a000;
+`
 
 const DeliveryCepContainer = styled.div`
   margin-top: 6px;
@@ -143,7 +273,7 @@ const DeliveryInputContent = styled.div`
 const DeliveryCepContent = styled.div`
   padding: 8px;
   align-items: center;
-  min-width: 422px;
+  width: 100%;
 
   h5 {
     margin-left: 150px;
@@ -152,7 +282,9 @@ const DeliveryCepContent = styled.div`
 
   div {
     display: flex;
-    justify-content: space-around;
+    padding-left: 30px;
+    padding-right: 30px;
+    justify-content: space-between;
   }
 
   span {
@@ -160,7 +292,13 @@ const DeliveryCepContent = styled.div`
   }
 `
 
-
+const ErrorCepContent = styled.p`
+  position: absolute;
+  font-size: 10px;
+  display: flex;
+  align-items: center;
+  color: #f00;
+`
 
 interface AsidePaymentCartContainerProps {
   $hasShipping: boolean;
@@ -168,48 +306,38 @@ interface AsidePaymentCartContainerProps {
 
 const AsidePaymentCartContainer = styled.aside<AsidePaymentCartContainerProps>`
   margin: 20px;
-  background-color: white;
   border-radius: 8px;
-  padding: 12px;
   height: ${(props) => (!props.$hasShipping ? "275px" : "unset")};
-
-  p,
-  h1,
-  h4 {
-    margin-bottom: 12px;
-  }
-  span {
-    margin-left: 4px;
-  }
-
-  /* svg {
-     height: 50px;
-     width: 50px;
-   } */
 
   #resume-buttons {
     display: flex;
     ${Buttons} {
-      margin-left: 12px;
-    }
-    #clear-cart-button {
-      background-color: red;
-      opacity: 0.9;
+      width: 100%;
     }
   }
 `;
 
 export {
   MainCartProducts,
+  MainContentCartProducts,
   PaymenteContainer,
   SectionPaymentCartContainer,
   ProductCartContainer,
   DeliveryCepContent,
   DeliveryInputContent,
   ButtonAdd,
+  OrderSummaryContent,
+  FormOfPaymentContent,
+  PaymentTicketContent,
+  PaymenteTicketTotal,
+  ProductTitle,
+  PaymentCartContent,
   ButtonReduce,
   ProductPrice,
+  TotalOrderSummary,
+  PaymentTicketContainer,
   ProductContentCart,
   DeliveryCepContainer,
+  ErrorCepContent,
   AsidePaymentCartContainer,
 };
